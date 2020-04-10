41d339b90d9d8d2cc8fdef39aa3cd704b7b229d2
王宁 <hongyan2703@163.com>
2020-04-11 02:19:09 +0800

    Add 添加gson

--- app/src/main/java/com/hongyan/life/activity/translate/TranslateFragment.java
+++ app/src/main/java/com/hongyan/life/activity/translate/TranslateFragment.java
@@ -6,18 +6,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.hongyan.life.R;
 import com.hongyan.life.activity.BaseFragment;
 import com.hongyan.life.net.LFHttpRequestUtils;
 import com.hongyan.life.net.LFNetworkCallback;
+import com.hongyan.life.utils.GsonUtils;
 
 import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -26,7 +26,12 @@ public class TranslateFragment extends BaseFragment {
     private View view;
     private EditText etInput;
     private ImageView imageSearch;
-    private TextView tvResult;
+    private TextView tvResultA;
+    private TextView tvResultA1;
+    private TextView tvResultB;
+    private TextView tvResultB1;
+    private TextView tvResultC1;
+    private TextView tvResultD1;
 
 
     @Override
@@ -34,7 +39,12 @@ public class TranslateFragment extends BaseFragment {
         view = inflater.inflate(R.layout.fragment_translate, container, false);
         etInput = view.findViewById(R.id.edit_input);
         imageSearch = view.findViewById(R.id.image_search);
-        tvResult = view.findViewById(R.id.tv_result);
+        tvResultA = view.findViewById(R.id.tv_result_A);
+        tvResultA1 = view.findViewById(R.id.tv_result_A1);
+        tvResultB = view.findViewById(R.id.tv_result_B);
+        tvResultB1 = view.findViewById(R.id.tv_result_B1);
+        tvResultC1 = view.findViewById(R.id.tv_result_C1);
+        tvResultD1 = view.findViewById(R.id.tv_result_D1);
         imageSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -52,7 +62,6 @@ public class TranslateFragment extends BaseFragment {
         }
     }
 
-
     private void search(String searchKey) {
         String url = null;
         try {
@@ -66,15 +75,29 @@ public class TranslateFragment extends BaseFragment {
         LFHttpRequestUtils.postSyn(url, parameterList, new LFNetworkCallback() {
             @Override
             public void completed(String response) {
-                tvResult.setText(response);
+                TranslateBean bean = GsonUtils.gsonResolve(response, TranslateBean.class);
+                tvResultA.setText(bean.query);
+                tvResultA1.setText(Arrays.toString(bean.translation));
+                tvResultB1.setText(bean.basic.phonetic);
+                tvResultC1.setText(Arrays.toString(bean.basic.explains));
+
+                StringBuilder builder = new StringBuilder();
+                ArrayList<TranslateBean.Web> web = bean.web;
+                for(TranslateBean.Web w: web){
+                    builder.append("·");
+                    builder.append(w.key);
+                    builder.append("\n");
+                    builder.append(Arrays.toString(w.value));
+                    builder.append("\n");
+                }
+                tvResultD1.setText(builder.toString());
             }
 
             @Override
             public void failed(int httpStatusCode, String error) {
-                tvResult.setText(error);
+                tvResultA.setText(error);
                 String errorMsg = "网络错误  [httpStatusCode" + httpStatusCode + "   error:" + error + "]";
             }
-
         });
     }
 
